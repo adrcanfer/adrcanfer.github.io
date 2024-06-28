433c6efcfb42bc173c5c57d4b308058da68b04e8
Adrián Cantón <adriancanton@gmail.com>
2024-06-28 19:42:14 +0200

    Fix load

--- assets/js/load.js
+++ assets/js/load.js
@@ -6,12 +6,13 @@ function getNavLanguage() {
 
 function loadWebsite() {
     var lang = getNavLanguage();
+    var currentPage = window.location.pathname;
 
-    // Verificar si el idioma es español
-    if (lang.startsWith('es')) {
+    // Verificar si el idioma es español y si no estamos ya en la página en español
+    if (lang.startsWith('es') && !currentPage.endsWith('es.html')) {
         window.location.href = 'es.html';
-    } else {
-        // Cualquier otro idioma se redirige a en.html
+    } else if (!lang.startsWith('es') && !currentPage.endsWith('en.html')) {
+        // Cualquier otro idioma se redirige a en.html, si no estamos ya en la página en inglés
         window.location.href = 'en.html';
     }
 }
